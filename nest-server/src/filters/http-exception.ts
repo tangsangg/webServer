424aef8ba4c42b249f6042bdcceaca634b55f864
chelsea.xu <chelsea.xu@corp.netease.com>
2022-08-03 15:36:59 +0800

    [update]

---
+++ nest-server/src/filters/http-exception.ts
@@ -0,0 +1,26 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
+import {  Response } from 'express';
+import { execPath } from 'process';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const message = exception.message
+
+    const exceptionResponse: any = exception.getResponse()
+    let validatorMessage = exceptionResponse
+    if (typeof validatorMessage === 'object') {
+      validatorMessage = exceptionResponse.message[0]
+    }
+
+    response
+      .status(status)
+      .json({
+        code: status,
+        message: validatorMessage || message,
+      });
+  }
+}
